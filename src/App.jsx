43180d8d24b688672cd5d0bb1887dc981ac8188e
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-12-18 12:40:45 +0200

    render the toys from firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
 import { collection, getDocs } from 'firebase/firestore'
 import { dbFireStore } from './config/firebase'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 function App() {
+
+  const [toys, setToys] = useState([])
 
   useEffect(() => {
     getFireStoreData()
   }, [])
 
   const getFireStoreData = async () => {
-    const ref = collection(dbFireStore, "toys")
-    const snapshot = await getDocs(ref)
-    // console.log(snapshot.docs)
-    const toysArray = []
-    snapshot.forEach((item) => {
-      toysArray.push({ id: item.id, ...item.data() })
-    })
-    console.log(toysArray)
+    try {
+      const ref = collection(dbFireStore, "toys")
+      const snapshot = await getDocs(ref)
+      // console.log(snapshot.docs)
+      const toysArray = []
+      snapshot.forEach((item) => {
+        toysArray.push({ id: item.id, ...item.data() })
+      })
+      setToys(toysArray)
+    } catch (error) {
+      alert('there was an Error try again')
+      console.log(error, 'ERROR!')
+    }
   }
 
   return (
-    <>
-      <h1 className='text-3xl text-red-500'>hello firestore</h1>
-    </>
+    <main className='bg-zinc-200 h-screen'>
+      <h1 className='text-4xl text-center'>Firestore Application</h1>
+      <ul>
+        {toys.map((toy) => {
+          return (
+            <div key={toy.id}>
+              <h1>{toy.name}</h1>
+            </div>
+          )
+        })}
+      </ul>
+    </main>
   )
 }
 
