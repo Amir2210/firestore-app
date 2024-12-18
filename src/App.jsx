48552dc1b55292020e2bb4d5af0bcbb893a3c304
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-12-18 12:18:47 +0200

    fetch data from firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
-
+import { collection, getDocs } from 'firebase/firestore'
+import { dbFireStore } from './config/firebase'
+import { useEffect } from 'react'
 function App() {
 
+  useEffect(() => {
+    getFireStoreData()
+  }, [])
+
+  const getFireStoreData = async () => {
+    const ref = collection(dbFireStore, "toys")
+    const snapshot = await getDocs(ref)
+    // console.log(snapshot.docs)
+    const toysArray = []
+    snapshot.forEach((item) => {
+      toysArray.push({ id: item.id, ...item.data() })
+    })
+    console.log(toysArray)
+  }
 
   return (
     <>
