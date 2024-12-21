cca7b3c7c1f315947a0029a893b6de6e4d66d58e
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-12-21 12:11:34 +0200

    Add back button to login and sign up page

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -31,6 +31,7 @@ export function Login() {
         <input ref={passwordRef} type="text" className='mx-2' />
         <h2 className='text-red-500'>{error}</h2>
         <button className='bg-blue-400 p-2 py-3 rounded-md text-white mt-4  shadow-md'>Login</button>
+        <button onClick={() => navigate('/')} className='bg-violet-400 p-2 py-3 rounded-md text-white mt-4  shadow-md'>Back</button>
       </form>
 
     </main>
