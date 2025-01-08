b3171ca5807c900ef2781d2811bba5f0d5ec1254
Amir Yankolovich <amiryankolovich2000@gmail.com>
2025-01-08 17:19:43 +0200

    Change the layout of toy list to grid

--- src/cmp/toysList.jsx
+++ src/cmp/toysList.jsx
@@ -2,7 +2,7 @@ import { ToyPreview } from './ToyPreview';
 
 export function ToyList({ toys, setIsModalOpen, setCurrentEditToy, userId, getFireStoreData }) {
   return (
-    <ul className='flex flex-col items-center mt-4'>
+    <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 items-center mt-4'>
       {toys.map((toy) => {
         return (
           <ToyPreview key={toy.id} toy={toy} setIsModalOpen={setIsModalOpen} setCurrentEditToy={setCurrentEditToy} userId={userId} getFireStoreData={getFireStoreData} />
